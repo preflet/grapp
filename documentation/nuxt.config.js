33da62c395ff7092e2dbbd9918f14fe4ec0b42ab
Mexson Fernandes <robomex2020@gmail.com>
2021-05-11 12:19:27 +0530

    :hammer: change in branch

--- documentation/nuxt.config.js
+++ documentation/nuxt.config.js
@@ -1,29 +1,29 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from "@nuxt/content-theme-docs";
 
 export default theme({
   docs: {
-    primaryColor: '#E24F55',
+    primaryColor: "#168ecf",
   },
   i18n: {
     locales: () => [
       {
-        code: 'fr',
-        iso: 'fr-FR',
-        file: 'fr-FR.js',
-        name: 'Français',
+        code: "fr",
+        iso: "fr-FR",
+        file: "fr-FR.js",
+        name: "Français",
       },
       {
-        code: 'en',
-        iso: 'en-US',
-        file: 'en-US.js',
-        name: 'English',
+        code: "en",
+        iso: "en-US",
+        file: "en-US.js",
+        name: "English",
       },
     ],
-    defaultLocale: 'en',
+    defaultLocale: "en",
   },
   pwa: {
     manifest: {
-      name: 'Grapp',
+      name: "Grapp",
     },
   },
-})
+});
